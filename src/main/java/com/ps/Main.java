321e64e3d3c07cbf8c3342d308862c55aef7936f
Dee S <dsmallwood0427@gmail.com>
2024-12-08 22:26:23 -0600

    Forgot to commit and push

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,4 +1,13 @@
 package com.ps;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class Main {
+    public static void main(String[] args) {
+        String username = args[0];
+        String password = args[1];
+
+        UserInterface.display(username, password);
+
+    }
 }
